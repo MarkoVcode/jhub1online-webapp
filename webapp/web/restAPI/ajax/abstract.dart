d4a79eea81c4a29ea362b7bf908e458573e3190c
marek.kaszewski@googlemail.com <marek.kaszewski@googlemail.com@5849219c-46f4-65c1-0d06-4bbd950a385a>
2014-07-05 19:06:06 +0000

    Preparing base for development

---
+++ webapp/web/restAPI/ajax/abstract.dart
@@ -0,0 +1,17 @@
+abstract class Jhub1Service {
+
+  static const String URL = "http://127.0.0.1";
+  static const String PORT = "8081";
+  static const String VERSION = "mock0";
+  
+  String buildURI() {
+    var buffer = new StringBuffer();
+    buffer.write(URL);
+    buffer.write(":");
+    buffer.write(PORT);
+    buffer.write("/");
+    buffer.write(VERSION);
+    buffer.write("/");
+    return buffer.toString();
+  }
+}
